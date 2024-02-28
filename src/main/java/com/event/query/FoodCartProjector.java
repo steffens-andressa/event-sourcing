ecbe689a935a2dae07e0ed6ff47d7e0a5185ba99
Andressa Steffens <steffensand@gmail.com>
2024-02-28 03:39:09 -0300

    Add FoodOrderingController to manage API calls and FoodCartProjector to query

---
+++ src/main/java/com/event/query/FoodCartProjector.java
@@ -0,0 +1,45 @@
+package com.event.query;
+
+import com.event.coreapi.FindFoodCartQuery;
+import com.event.coreapi.FoodCartCreatedEvent;
+import com.event.coreapi.ProductDeselectedEvent;
+import com.event.coreapi.ProductSelectedEvent;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+public class FoodCartProjector {
+    private final FoodCartViewRepository foodCartViewRepository;
+
+    public FoodCartProjector(FoodCartViewRepository foodCartViewRepository){
+        this.foodCartViewRepository = foodCartViewRepository;
+    }
+
+    @EventHandler
+    public void on(FoodCartCreatedEvent event){
+        FoodCartView foodCartView = new FoodCartView(event.getFoodCartId(), Collections.emptyMap());
+        foodCartViewRepository.save(foodCartView);
+    }
+
+    @EventHandler
+    public void on(ProductSelectedEvent event){
+        foodCartViewRepository.findById(event.getFoodCartId()).ifPresent(
+            foodCartView -> foodCartView.addProducts(event.getProductId(), event.getQuantity())
+        );
+    }
+
+    @EventHandler
+    public void on(ProductDeselectedEvent event){
+        foodCartViewRepository.findById(event.getProductId()).ifPresent(
+            foodCartView -> foodCartView.removeProducts(event.getProductId(), event.getQuantity())
+        );
+    }
+
+    @QueryHandler
+    public FoodCartView handle(FindFoodCartQuery query){
+        return foodCartViewRepository.findById(query.getFoodCartId()).orElse(null);
+    }
+}
