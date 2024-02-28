741629bddb7e45bbe6b07c63d08710cc5b2ee3ab
Andressa Steffens <steffensand@gmail.com>
2024-02-28 03:35:59 -0300

    Create FoodCartView, add functions to add, remove products and persist them using JpaRepository

---
+++ src/main/java/com/event/query/FoodCartView.kt
@@ -0,0 +1,30 @@
+package com.event.query
+
+import jdk.jfr.DataAmount
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.HashMap
+import java.util.UUID
+import javax.persistence.ElementCollection
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.Id
+
+@Entity
+data class FoodCartView(
+    @Id val foodCartId: UUID,
+    @ElementCollection(fetch = FetchType.EAGER) val products: MutableMap<UUID, Int>
+){
+    constructor() : this(UUID.randomUUID(), HashMap<UUID, Int>()){}
+
+    fun addProducts(productId: UUID, amount: Int) =
+        products.compute(productId) {_,quantity -> (quantity?: 0) + amount}
+
+    fun removeProducts(productId: UUID, amount: Int) {
+        val leftOverQuantity = products.compute(productId) {_,quantity -> (quantity?: 0) - amount}
+        if (leftOverQuantity == 0){
+            products.remove(productId)
+        }
+    }
+}
+
+interface FoodCartViewRepository: JpaRepository<FoodCartView,UUID>
